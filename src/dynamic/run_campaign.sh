7b364871dd624965e03b1b369d4c55adac30a4fe
prashast <srivas41@purdue.edu>
2023-11-11 18:12:44 +0000

    Fix run campaign script

--- src/dynamic/run_campaign.sh
+++ src/dynamic/run_campaign.sh
@@ -8,9 +8,8 @@
 FUZZER_DIR="/root/SeriFuzz/jazzer"
 MODE_FILE="$FUZZER_DIR/_crystallizer_mode"
 LIB_FILE="$FUZZER_DIR/_libname"
-CPU_NUM=$1
-MODE=$2
-LIB=$3
+MODE=$1
+LIB=$2
 cd $FUZZER_DIR
 
 rm -f _initTimeStamp _covStallTime _overThreshold _lastSeenCovTime fuzzProgress.list serifuzz.log* test.txt crashes/* && bazel build //:jazzer_release && tar -xf bazel-bin/jazzer_release.tar.gz -C bazel-bin/ && bazel build //examples:examples 
@@ -19,7 +18,7 @@ echo -n $MODE > $MODE_FILE
 echo -n $LIB > $LIB_FILE 
 
 echo "./bazel-bin/jazzer --cp=./bazel-bin/examples/SeriFuzz_target_deploy.jar:/root/SeriFuzz/src/static/sootOutput/out.jar --target_class=com.example.SeriFuzz --instrumentation_excludes=org.jgrapht.**::org.apache.log4j.**::javassist.** --custom_hooks=com.example.RuntimeFuzzerHooks --custom_hook_excludes=javassist.** -timeout=5 -rss_limit_mb=8096" 
-CMD="taskset -c $CPU_NUM ./bazel-bin/jazzer --cp=./bazel-bin/examples/SeriFuzz_target_deploy.jar:/root/SeriFuzz/src/static/sootOutput/out.jar --target_class=com.example.SeriFuzz --instrumentation_excludes=org.jgrapht.**::org.apache.log4j.**::javassist.** --keep_going=5000 -timeout=60 -rss_limit_mb=8096" 
+CMD="./bazel-bin/jazzer --cp=./bazel-bin/examples/SeriFuzz_target_deploy.jar:/root/SeriFuzz/src/static/sootOutput/out.jar --target_class=com.example.SeriFuzz --instrumentation_excludes=org.jgrapht.**::org.apache.log4j.**::javassist.** --keep_going=5000 -timeout=60 -rss_limit_mb=8096" 
 while true
 do
     echo "$PWD"
